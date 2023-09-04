91ba0b180ffd1bd4eb433e1b7e3c57632a8b6ee1
farhanahmedhasan <achasandjmamunxyz2015@gmail.com>
2023-09-04 16:55:01 +0600

    added-persistent-default-layout

--- resources/js/Main.jsx
+++ resources/js/Main.jsx
@@ -1,11 +1,15 @@
 import { createInertiaApp } from "@inertiajs/react"
 import { createRoot } from "react-dom/client"
+import Layout from "@/Shared/Layout.jsx"
 import React from "react"
 
 createInertiaApp({
-    resolve: (name) => {
+    resolve: async (name) => {
         const pages = import.meta.glob("./Pages/**/*.jsx", { eager: false })
-        return pages[`./Pages/${name}.jsx`]()
+        let page = await pages[`./Pages/${name}.jsx`]()
+
+        page.default.layout = page.default.layout || ((page) => <Layout children={page} />)
+        return page
     },
     setup({ el, App, props }) {
         createRoot(el).render(
