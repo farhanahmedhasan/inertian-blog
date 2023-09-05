4da45448f7707c58960717cdaa77d367b3ba03dd
farhanahmedhasan <achasandjmamunxyz2015@gmail.com>
2023-09-05 11:14:11 +0600

    added-dynamic-meta-description

--- resources/js/Pages/Contact/Index.jsx
+++ resources/js/Pages/Contact/Index.jsx
@@ -1,8 +1,12 @@
 import React from "react"
+import { Head } from "@inertiajs/react"
 
 export default function Index() {
     return (
         <>
+            <Head>
+                <meta head-key="description" name="description" content="This is contact meta description" />
+            </Head>
             <div>
                 <h1>This is a contact page</h1>
             </div>
--- resources/js/Shared/Layout.jsx
+++ resources/js/Shared/Layout.jsx
@@ -1,11 +1,15 @@
 import Footer from "@/Shared/Footer.jsx"
 import Nav from "@/Shared/Nav.jsx"
 import React from "react"
+import { Head } from "@inertiajs/react"
 
 export default function Layout({ children }) {
     return (
         <>
             <Nav />
+            <Head>
+                <meta head-key="description" name="description" content="This is default meta description" />
+            </Head>
             <main>
                 <iframe
                     width="560"
