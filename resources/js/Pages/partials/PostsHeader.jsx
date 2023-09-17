ee9acad5375904e2b842b49d85ccdfeef8791d39
farhanahmedhasan <achasandjmamunxyz2015@gmail.com>
2023-09-17 14:32:04 +0600

    extracted-a-arrow-down-component

--- resources/js/Pages/partials/PostsHeader.jsx
+++ resources/js/Pages/partials/PostsHeader.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react"
 import { router } from "@inertiajs/react"
+import ArrowDown from "@/asstes/svgs/ArrowDown.jsx"
 
 export default function PostsHeader({ categories }) {
     const [catValue, setCatValue] = useState("")
@@ -58,21 +59,7 @@ export default function PostsHeader({ categories }) {
                         })}
                     </select>
 
-                    <svg
-                        className="transform -rotate-90 absolute pointer-events-none"
-                        style={{ right: "12px" }}
-                        width="22"
-                        height="22"
-                        viewBox="0 0 22 22"
-                    >
-                        <g fill="none" fillRule="evenodd">
-                            <path stroke="#000" strokeOpacity=".012" strokeWidth=".5" d="M21 1v20.16H.84V1z"></path>
-                            <path
-                                fill="#222"
-                                d="M13.854 7.224l-3.847 3.856 3.847 3.856-1.184 1.184-5.04-5.04 5.04-5.04z"
-                            ></path>
-                        </g>
-                    </svg>
+                    <ArrowDown />
                 </div>
 
                 {/*Other Filters*/}
@@ -88,21 +75,7 @@ export default function PostsHeader({ categories }) {
                         <option value="bar">Bar</option>
                     </select>
 
-                    <svg
-                        className="transform -rotate-90 absolute pointer-events-none"
-                        style={{ right: "12px" }}
-                        width="22"
-                        height="22"
-                        viewBox="0 0 22 22"
-                    >
-                        <g fill="none" fillRule="evenodd">
-                            <path stroke="#000" strokeOpacity=".012" strokeWidth=".5" d="M21 1v20.16H.84V1z"></path>
-                            <path
-                                fill="#222"
-                                d="M13.854 7.224l-3.847 3.856 3.847 3.856-1.184 1.184-5.04-5.04 5.04-5.04z"
-                            ></path>
-                        </g>
-                    </svg>
+                    <ArrowDown />
                 </div>
 
                 {/*Search*/}
