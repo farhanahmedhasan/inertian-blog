7b7dd6f1538b77f61f03a615dcdf11219a9ba050
farhanahmedhasan <achasandjmamunxyz2015@gmail.com>
2023-10-30 09:20:09 +0600

    added comment table factory seeder model controller and updated table factory and seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Comment;
 use Illuminate\Database\Seeder;
 use App\Models\Category;
 use App\Models\Post;
@@ -16,12 +17,23 @@ class DatabaseSeeder extends Seeder
     {
         User::factory()->create([
             'username' => "mehar",
+            'email' => "mehar@gmail.com",
             'name' => "Humayra Binte Mehar"
         ]);
-        User::factory(9)->create();
+
+        User::factory()->create([
+            'username' => "yourHasan",
+            'name' => "Hasan Al Mamun",
+            'email' => "dev.hasanalmamun@gmail.com",
+            'password' => "yourHasan"
+        ]);
+
+        User::factory(8)->create();
 
         Category::factory(10)->create();
 
         Post::factory(50)->create();
+
+        Comment::factory(700)->create();
     }
 }
